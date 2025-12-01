8d116a47fa0fbce64f1f3abf472b90138a92c127
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-11-30 21:08:57 -0300

    Remove outdated sections on Comparison Operators, Existence and Booleans, and Default Values; update app.js to a simple arithmetic example; add new content for Object Literals and Coercion.

--- app.js
+++ app.js
@@ -1,16 +1,2 @@
-// long running function
-function waitThreeSeconds() {
-  var ms = 3000 + new Date().getTime();
-  while (new Date() < ms) {}
-  console.log("finished function");
-}
-
-function clickHandler() {
-  console.log("click event!");
-}
-
-// listen for the click event
-document.addEventListener("click", clickHandler);
-
-waitThreeSeconds();
-console.log("finished execution");
+var a = 3 + 4 * 5;
+console.log(a);
